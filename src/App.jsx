5b6a7af49ffb13a383a80523ab1028e7441dda02
Stanislas <simostanley91@gmail.com>
2025-02-27 15:29:13 +0100

    Début du portfolio

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+const App  = () => {
+  return (
+    <div className="w-screen h-screen flex justify-center items-center">
+      <h1 className="font-bold text-2xl text-blue-500">React + Vite + Tailwindcss V 4.0.9</h1>
+    </div>
+  )
+}
+export default App
